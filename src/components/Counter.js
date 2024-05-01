e9c6c7fdd1c88b38473f48b9cef90680392a06b0
oliullahlimon <oliullah.limon.55@gmail.com>
2024-05-01 15:00:02 +0600

    Assignment 2 is completed.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,30 @@
-import React from 'react';
+/* eslint-disable prettier/prettier */
+import React, { useState } from 'react';
 
 const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount((count) => count + 1);
+
+  const decrement = () => setCount((count) => count - 1);
+
+  const reset = () => setCount((count) => count * 0);
+
   return (
     <div className="counter center">
       <h1 className="counter__title">Counter App</h1>
       <div className="card center">
-        <h2 className="card__title">Count : 0</h2>
+        <h2 className="card__title">Count : {count}</h2>
         <div className="card__btns">
-          <button className="btn card__btn">+</button>
-          <button className="btn card__btn">-</button>
-          <button className="btn card__btn">0</button>
+          <button onClick={increment} disabled={count === 5} className="btn card__btn">
+            +
+          </button>
+          <button onClick={decrement} disabled={count === -5} className="btn card__btn">
+            -
+          </button>
+          <button onClick={reset} className="btn card__btn">
+            0
+          </button>
         </div>
       </div>
     </div>
